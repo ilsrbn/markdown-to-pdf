7407a96da5abe74f93ae93007c7889dff7939cca
Illia Serbin <i@serbin.co>
2024-05-18 13:11:55 +0300

    Initial commit

---
+++ lua/markdown-to-pdf.lua
@@ -0,0 +1,44 @@
+local M = {}
+
+local uv = vim.loop
+
+M.run_pandoc = function()
+  local file = vim.fn.expand('%:p')
+  local file_base = vim.fn.expand('%:t:r')
+  local pdf_file = file_base .. '.pdf'
+
+  local stdout = uv.new_pipe(false)
+  local stderr = uv.new_pipe(false)
+  local handle
+
+  handle = uv.spawn('pandoc', {
+    args = { file, '-o', pdf_file },
+    stdio = { nil, stdout, stderr }
+  }, function(code, signal)
+    stdout:close()
+    stderr:close()
+    handle:close()
+    if code == 0 then
+      print("Pandoc conversion successful!")
+      vim.fn.jobstart({ 'xdg-open', pdf_file }, { detach = true })
+    else
+      print("Pandoc conversion failed with code " .. code .. " and signal " .. signal)
+    end
+  end)
+
+  uv.read_start(stdout, function(err, data)
+    assert(not err, err)
+    if data then
+      print("stdout: " .. data)
+    end
+  end)
+
+  uv.read_start(stderr, function(err, data)
+    assert(not err, err)
+    if data then
+      print("stderr: " .. data)
+    end
+  end)
+end
+
+return M
---
+++ plugin/markdown-to-pdf.vim
@@ -0,0 +1,3 @@
+if has('nvim')
+  lua require('my_pandoc_plugin').run_pandoc()
+endif
